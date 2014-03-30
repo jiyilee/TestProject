6f86cc1bb3a77e24ad3cf583106651ae67f621b8
jiyilee <jiyilee@qq.com>
2014-03-30 15:19:38 +0800

    add testProject

---
+++ TestCSharpConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitTest();
+            foreach (var item in GetTestListItem())
+            {
+                item.Name += item.Name;
+                //testList.TestList.Add(new TestClass() { ID="inter" });
+            } 
+        }
+
+        static void InitTest()
+        {
+            testList = new TestClassList()
+            {
+                ID = "id",
+                Name = "name",
+                TestList = new List<TestClass>() {
+                 new TestClass{ ID="interID1", Name="interName1" },
+                 new TestClass{ ID="interID2", Name="interName2" },
+                 new TestClass{ ID="interID3", Name="interName3" }
+                }
+            };
+        }
+
+        static IEnumerable<TestClass> GetTestListItem()
+        {
+            return testList.TestList.Where(x => x.ID.StartsWith("inter"));
+        }
+
+        static TestClassList testList;
+    }
+
+
+
+    class TestClassList
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<TestClass> TestList { get; set; }
+    }
+
+    class TestClass
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
